2ce3986169dab40ecfe61dafd4bb4d981d32f070
btderr <btderr@oakland.edu>
2018-02-28 22:03:46 -0500

    Sprint #4 Progress

---
+++ app/src/main/java/com/example/mims/mimsmobileclient/LEHomepageActivity.java
@@ -0,0 +1,13 @@
+package com.example.mims.mimsmobileclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LEHomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lehomepage);
+    }
+}
---
+++ app/src/main/java/com/example/mims/mimsmobileclient/LEViewReportActivity.java
@@ -0,0 +1,13 @@
+package com.example.mims.mimsmobileclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LEViewReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leview_report);
+    }
+}
