3dcc96f3c79a7b8782a26f3209405edfeca51de3
btderr <btderr@oakland.edu>
2018-03-14 22:51:33 -0400

    Sprint #5 Progress

---
+++ app/src/main/java/com/example/mims/mimsmobileclient/AssignReportNumber.java
@@ -0,0 +1,13 @@
+package com.example.mims.mimsmobileclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AssignReportNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assign_report_number);
+    }
+}
