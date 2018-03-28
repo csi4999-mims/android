b1220e42f4b02c2c9de2d7e0b900092bdc3fbbef
btderr <btderr@oakland.edu>
2018-03-28 13:02:21 -0400

    Sprint #6 Progress

---
+++ app/src/main/java/com/example/mims/mimsmobileclient/WelcomePage.java
@@ -0,0 +1,33 @@
+package com.example.mims.mimsmobileclient;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.net.URL;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+
+
+        final Button startButton = (Button)findViewById(R.id.StartButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent viewIntent =
+                        new Intent("android.intent.action.VIEW",
+                                Uri.parse("https://csi4999mims.online/"));
+                startActivity(viewIntent);
+            }
+        });
+
+
+    }
+}
